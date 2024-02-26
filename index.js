d238e240a78ba2dcbb549d68c72d4b59b179d5dc
Veritiums <161390996+Veritiums@users.noreply.github.com>
2024-02-27 10:14:49 +1100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching Suport","Watching trdemarked"];
 
 
 let currentIndex = 0;
